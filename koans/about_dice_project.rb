ef53286e3d29848ba7167e2fc2d3feafcfdcad8c
Antonio Hernández Blas <1096022+nihilismus@users.noreply.github.com>
2020-09-05 22:36:59 -0500

    Make GREEN about_dice_project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  def roll(times)
+    values = []
+    (1..times).each do |_|
+      values << rand(1..6)
+    end
+    @values = values
+  end
+
+  def values
+    @values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
